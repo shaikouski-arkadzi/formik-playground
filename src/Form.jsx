bf314999cfb0723a5ab2ce52924960dfe6f23fc1
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-13 21:49:09 +0300

    Add initialValues and connect Formik with form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,24 +1,70 @@
+import { useFormik } from "formik";
+
 const Form = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      amount: 0,
+      currency: "",
+      text: "",
+      terms: false,
+    },
+  });
+
   return (
     <form className="form">
       <h2>Отправить пожертвование</h2>
       <label htmlFor="name">Ваше имя</label>
-      <input id="name" name="name" type="text" />
+      <input
+        id="name"
+        name="name"
+        type="text"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+      />
       <label htmlFor="email">Ваша почта</label>
-      <input id="email" name="email" type="email" />
+      <input
+        id="email"
+        name="email"
+        type="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+      />
       <label htmlFor="amount">Количество</label>
-      <input id="amount" name="amount" type="number" />
+      <input
+        id="amount"
+        name="amount"
+        type="number"
+        value={formik.values.amount}
+        onChange={formik.handleChange}
+      />
       <label htmlFor="currency">Валюта</label>
-      <select id="currency" name="currency">
+      <select
+        id="currency"
+        name="currency"
+        value={formik.values.currency}
+        onChange={formik.handleChange}
+      >
         <option value="">Выберите валюту</option>
         <option value="USD">USD</option>
         <option value="UAH">UAH</option>
         <option value="RUB">RUB</option>
       </select>
       <label htmlFor="text">Ваше сообщение</label>
-      <textarea id="text" name="text" />
+      <textarea
+        id="text"
+        name="text"
+        value={formik.values.text}
+        onChange={formik.handleChange}
+      />
       <label className="checkbox">
-        <input name="terms" type="checkbox" />
+        <input
+          name="terms"
+          type="checkbox"
+          value={formik.values.terms}
+          onChange={formik.handleChange}
+        />
         Соглашаетесь с политикой конфиденциальности?
       </label>
       <button type="submit">Отправить</button>
