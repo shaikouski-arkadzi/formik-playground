64f4ebca60bb00f5fad0dde9cb184b6e45899faf
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-10-15 20:58:30 +0300

    Add second variant using Formik components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Form from "./Form";
+import FormSecond from "./FormSecond";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Form />
+      <FormSecond />
     </div>
   );
 }
---
+++ src/FormSecond.jsx
@@ -0,0 +1,71 @@
+import { Form, Field, ErrorMessage, Formik } from "formik";
+import {
+  object as YUPObject,
+  string as YUPString,
+  number as YUPNumber,
+  boolean as YUPBoolean,
+} from "yup";
+
+const FormSecond = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        amount: 0,
+        currency: "",
+        text: "",
+        terms: false,
+      }}
+      validationSchema={YUPObject({
+        name: YUPString().min(2, "2 letters minimum").required("Required"),
+        email: YUPString().email("Wrong email format").required("Required"),
+        amount: YUPNumber().required("Required"),
+        currency: YUPString().required("Required"),
+        text: YUPString(),
+        terms: YUPBoolean()
+          .required("Required")
+          .oneOf([true], "Must be confirmed"),
+      })}
+    >
+      <Form className="form">
+        <h2>Отправить пожертвование</h2>
+
+        <label htmlFor="name">Ваше имя</label>
+        <Field id="name" name="name" type="text" />
+        <ErrorMessage className="error" name="name" />
+
+        <label htmlFor="email">Ваша почта</label>
+        <Field id="email" name="email" />
+        <ErrorMessage className="error" name="email" />
+
+        <label htmlFor="amount">Количество</label>
+        <Field id="amount" name="amount" type="number" />
+        <ErrorMessage className="error" name="amount" />
+
+        <label htmlFor="currency">Валюта</label>
+        <Field id="currency" name="currency" as="select">
+          <option value="">Выберите валюту</option>
+          <option value="USD">USD</option>
+          <option value="UAH">UAH</option>
+          <option value="RUB">RUB</option>
+        </Field>
+        <ErrorMessage className="error" name="currency" />
+
+        <label htmlFor="text">Ваше сообщение</label>
+        <Field id="text" name="text" as="textarea" />
+        <ErrorMessage className="error" name="text" />
+
+        <label className="checkbox">
+          <Field name="terms" type="checkbox" />
+          Соглашаетесь с политикой конфиденциальности?
+        </label>
+        <ErrorMessage className="error" name="terms" />
+
+        <button type="submit">Отправить</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormSecond;
